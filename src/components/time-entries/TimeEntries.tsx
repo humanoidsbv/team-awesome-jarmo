477c7d231b4c215a6bf83c83f2bdea8da38eab11
Jarmo van der Heul <jarmovdh@gmail.com>
2022-10-12 10:38:37 +0200

    timeEntry filter by client

--- src/components/time-entries/TimeEntries.tsx
+++ src/components/time-entries/TimeEntries.tsx
@@ -1,4 +1,5 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+
 import * as Styled from "./TimeEntries.styled";
 import { TimeEntry } from "../time-entry/TimeEntry";
 import * as Types from "../../types/types";
@@ -11,6 +12,16 @@ export const TimeEntries = ({ isModalActive, handleModal }: Types.AtBuildProps) 
   const { timeEntries, setTimeEntries } = useContext(EntriesContext);
   const [sortOrder, setSortOrder] = useState("ascending");
   const [sortedTimeEntries, setSortedTimeEntries] = useState(timeEntries);
+  const [selectedClient, setSelectedClient] = useState("");
+
+  const filterClients = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedClient(event.target.value);
+  };
+
+  const timeClients = timeEntries.map((entry) => entry.client);
+  const uniqueClients = timeClients.filter((client, index) => {
+    return timeClients.indexOf(client) === index;
+  });
 
   const removeEntry = (entry: Types.EntryApiProps) => {
     const updatedEntries = timeEntries.filter((timeEntry) => timeEntry.id !== entry.id);
@@ -43,48 +54,59 @@ export const TimeEntries = ({ isModalActive, handleModal }: Types.AtBuildProps) 
     <>
       <Styled.Main>
         <Styled.Container>
+          <Styled.Label>Filter by client: </Styled.Label>
+          <Styled.Select value={selectedClient} onChange={filterClients}>
+            <option disabled value="">
+              --Choose a client--
+            </option>
+            {uniqueClients.map((entry) => (
+              <option value={entry} label={entry} key={entry} />
+            ))}
+          </Styled.Select>
           <Styled.Label>Sort by Date:</Styled.Label>
           <Styled.Select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
             <option disabled value="">
               --Select option--
             </option>
-            {sortOptions.map((label, i) => (
-              <option key={label + i} label={label} value={label}>
+            {sortOptions.map((label) => (
+              <option key={label} label={label} value={label}>
                 {label}
               </option>
             ))}
           </Styled.Select>
         </Styled.Container>
-        {sortedTimeEntries.map((timeEntry, i, arr) => {
-          const currentDate = new Date(timeEntry.startTime).toLocaleDateString("en-EN", {
-            weekday: "long",
-            day: "numeric",
-            month: "numeric",
-          });
+        {sortedTimeEntries
+          .filter((entry) => selectedClient === "" || entry.client === selectedClient)
+          .map((timeEntry, i, arr) => {
+            const currentDate = new Date(timeEntry.startTime).toLocaleDateString("en-EN", {
+              weekday: "long",
+              day: "numeric",
+              month: "numeric",
+            });
 
-          const previousDate = new Date(arr[i - 1]?.startTime).toLocaleDateString("en-EN", {
-            weekday: "long",
-            day: "numeric",
-            month: "numeric",
-          });
+            const previousDate = new Date(arr[i - 1]?.startTime).toLocaleDateString("en-EN", {
+              weekday: "long",
+              day: "numeric",
+              month: "numeric",
+            });
 
-          return (
-            <div key={timeEntry.id}>
-              {previousDate !== currentDate && (
-                <Styled.DayContainer>
-                  <Styled.Title>{currentDate}</Styled.Title>
-                  <Styled.Title>08:00</Styled.Title>
-                </Styled.DayContainer>
-              )}
-              <TimeEntry
-                client={timeEntry.client}
-                startTime={timeEntry.startTime}
-                stopTime={timeEntry.endTime}
-                removeEntry={() => removeEntry(timeEntry)}
-              />
-            </div>
-          );
-        })}
+            return (
+              <div key={timeEntry.id}>
+                {previousDate !== currentDate && (
+                  <Styled.DayContainer>
+                    <Styled.Title>{currentDate}</Styled.Title>
+                    <Styled.Title>08:00</Styled.Title>
+                  </Styled.DayContainer>
+                )}
+                <TimeEntry
+                  client={timeEntry.client}
+                  startTime={timeEntry.startTime}
+                  stopTime={timeEntry.endTime}
+                  removeEntry={() => removeEntry(timeEntry)}
+                />
+              </div>
+            );
+          })}
         <Modal isActive={isModalActive} title="New Time Entry" onClose={() => handleModal()}>
           <TimeEntryForm
             timeEntries={timeEntries}
