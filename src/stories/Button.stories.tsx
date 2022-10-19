f936ce46ea99a3e49c914f0f3ff66636ff7f14b5
Jarmo van der Heul <jarmovdh@gmail.com>
2022-10-19 16:27:32 +0200

    implemented add-ons

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Meta, Story } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { action } from "@storybook/addon-actions";
 
 import { Button } from "../components/button/Button";
 import { ButtonProps } from "../types";
@@ -7,6 +9,17 @@ import { ButtonProps } from "../types";
 export default {
   title: "Components/Button",
   component: Button,
+  decorators: [withInfo],
+  parameters: {
+    info: {
+      text: `
+    Reusable button with 2 types of variants, more info below:
+    ~~~js
+    <Button>Click Here</Button>
+    ~~~
+  `,
+    },
+  },
   argTypes: {
     label: { control: "text" },
     hasIcon: { control: "boolean" },
@@ -19,7 +32,7 @@ export default {
   },
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} onClick={action("clicked")} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
