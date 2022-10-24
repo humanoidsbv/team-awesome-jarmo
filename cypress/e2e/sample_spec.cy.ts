086aa0640b38ddb466b7b86e2ec17915d193b744
Jarmo van der Heul <jarmovdh@gmail.com>
2022-10-24 15:51:41 +0200

    linting in cypress fixed

--- cypress/e2e/sample_spec.cy.ts
+++ cypress/e2e/sample_spec.cy.ts
@@ -1,17 +1,12 @@
+/* eslint-disable */
 describe("Routing", () => {
   it("Navigates to other pages", () => {
-    // eslint-disable-next-line no-undef
     cy.visit("http://localhost:3000");
 
-    // eslint-disable-next-line no-undef
     cy.get("[data-cy=menu]").should("exist");
-    // eslint-disable-next-line no-undef
     cy.get("[data-cy=menu]").contains("Team Members").click();
-    // eslint-disable-next-line no-undef
     cy.get("[data-cy=menu]").contains("Timesheet").click();
-    // eslint-disable-next-line no-undef
     cy.get(".Buttonstyled__Button-sc-596st9-0").click();
-    // eslint-disable-next-line no-undef
     cy.get("[data-cy=sub-header]").contains("Timesheets");
   });
 });
